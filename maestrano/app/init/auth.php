cad4d737fe67c0fe2ce4a7fada5baa4d6ad1805e
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-02-18 17:04:40 +1100

    configured api

--- maestrano/app/init/auth.php
+++ maestrano/app/init/auth.php
@@ -10,9 +10,56 @@ require MAESTRANO_ROOT . '/app/init/base.php';
 //-----------------------------------------------
 // Require your app specific files here
 //-----------------------------------------------
-//define('MY_APP_DIR', realpath(MAESTRANO_ROOT . '/../'));
+define('APP_DIR', realpath(MAESTRANO_ROOT . '/../'));
+chdir(APP_DIR);
 //require MY_APP_DIR . '/include/some_class_file.php';
 //require MY_APP_DIR . '/config/some_database_config_file.php';
+
+
+//-----------------------------------------------
+// Interleave init code
+//-----------------------------------------------
+$GLOBALS['PATHTOINTERLEAVE'] = APP_DIR . '/';
+
+if ($_SERVER['HTTPS'] == "on") {
+	$_GLOBALS['SecureCookie'] = 1;
+} else {
+	$_GLOBALS['SecureCookie'] = 0;
+}
+
+if (!$GLOBALS['CONFIGFILE']) {
+	$GLOBALS['CONFIGFILE'] = $GLOBALS['PATHTOINTERLEAVE'] . "config/config.inc.php";
+}
+require_once($GLOBALS['PATHTOINTERLEAVE'] . "functions.php");
+require_once($GLOBALS['CONFIGFILE']);
+require_once($GLOBALS['PATHTOINTERLEAVE'] . "config/config-vars.php");
+
+
+// This is done so we can use this var without htme later on. These vars should never contain characters which should be
+// encoded so this is only for security (if someone starts messing around)
+
+if (isset($_REQUEST['AjaxHandler']))	$_REQUEST['AjaxHandler'] = htme($_REQUEST['AjaxHandler']);
+if (isset($_REQUEST['e']))				$_REQUEST['e']			 = htme($_REQUEST['e']);
+if (isset($_REQUEST['eid']))			$_REQUEST['eid']		 = htme($_REQUEST['eid']);
+if (isset($_REQUEST['id']))				$_REQUEST['id']			 = htme($_REQUEST['id']);
+if (isset($_REQUEST['recordid']))		$_REQUEST['recordid']	 = htme($_REQUEST['recordid']);
+if (isset($_REQUEST['templateid']))		$_REQUEST['templateid']	 = htme($_REQUEST['templateid']);
+if (isset($_REQUEST['fileid']))			$_REQUEST['fileid']		 = htme($_REQUEST['fileid']);
+if (isset($_REQUEST['tid']))			$_REQUEST['tid']		 = htme($_REQUEST['tid']);
+
+
+// Set interleave repository	
+if ($_REQUEST['repository'] == "" && $_REQUEST['repositoryToLoginTo'] == "") {
+	$_REQUEST['repositoryToLoginTo'] = "0";
+}
+
+// Set a global variable called $db
+if (DB_Connect($_REQUEST['repository'], false)) {
+	SwitchToRepos($_REQUEST['repository']);
+}
+
+// Flag the app as initiated
+$GLOBALS['INITIATED'] = true;
 
 //-----------------------------------------------
 // Perform your custom preparation code
@@ -21,12 +68,9 @@ require MAESTRANO_ROOT . '/app/init/base.php';
 // automatically be passed to the MnoSsoUser object
 // for construction
 // e.g:
-// $opts = array();
-// if (!empty($db_name) and !empty($db_user)) {
-//     $conn = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_user, $db_pass);
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-//     
-//     $opts['db_connection'] = $conn;
-// }
+$opts = array();
+global $db;
+$opts['db_connection'] = $db;
+
 
 
--- maestrano/app/sso/MnoSsoUser.php
+++ maestrano/app/sso/MnoSsoUser.php
@@ -37,29 +37,22 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return boolean whether the user was successfully set in session or not
    */
-  // protected function setInSession()
-  // {
-  //   // First set $conn variable (need global variable?)
-  //   $conn = $this->connection;
-  //   
-  //   $sel1 = $conn->query("SELECT ID,name,lastlogin FROM user WHERE ID = $this->local_id");
-  //   $chk = $sel1->fetch();
-  //   if ($chk["ID"] != "") {
-  //       $now = time();
-  //       
-  //       // Set session
-  //       $this->session['userid'] = $chk['ID'];
-  //       $this->session['username'] = stripslashes($chk['name']);
-  //       $this->session['lastlogin'] = $now;
-  //       
-  //       // Update last login timestamp
-  //       $upd1 = $conn->query("UPDATE user SET lastlogin = '$now' WHERE ID = $this->local_id");
-  //       
-  //       return true;
-  //   } else {
-  //       return false;
-  //   }
-  // }
+  protected function setInSession()
+  {
+    if ($this->local_id) {
+        
+				GenerateSecret($this->uid);
+				InitUser($this->uid);
+				SetAttribute("user", "LastActivity", date('U'), $this->local_id);
+				ProcessTriggers("user_login", false, false, false, false);
+				ThingsToDoAtLogin();
+        setcookie('session',$_COOKIE['session'],0,'/');
+        
+        return true;
+    } else {
+        return false;
+    }
+  }
   
   
   /**
@@ -69,52 +62,120 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return the ID of the user created, null otherwise
    */
-  // protected function createLocalUser()
-  // {
-  //   $lid = null;
-  //   
-  //   if ($this->accessScope() == 'private') {
-  //     // First set $conn variable (need global variable?)
-  //     $conn = $this->connection;
-  //     
-  //     // Create user
-  //     $lid = $this->connection->query("CREATE BLA.....");
-  //   }
-  //   
-  //   return $lid;
-  // }
+  protected function createLocalUser()
+  {
+    $lid = null;
+    
+    if ($this->accessScope() == 'private') {
+      $user_data = $this->buildLocalUser();
+      $sql = sprintf("INSERT INTO CRMloginusers(name,password,FULLNAME,EMAIL,FORCEPASSCHANGE,LASTPASSCHANGE,administrator) VALUES('%s',PASSWORD('%s'),'%s','%s','%s','%s','%s')",
+        mysql_real_escape_string($user_data['name']),
+        mysql_real_escape_string($user_data['password']),
+        mysql_real_escape_string($user_data['fullname']),
+        mysql_real_escape_string($user_data['email']),
+        mysql_real_escape_string($user_data['forcepasswordchange']),
+        mysql_real_escape_string($user_data['lastpasschange']),
+        mysql_real_escape_string($user_data['administrator'])
+      );
+			$q = mysql_query($sql,$this->connection);
+      
+      if ($q) {
+        $lid = mysql_insert_id();
+      }
+    }
+    
+    return $lid;
+  }
+  
+  /**
+   * Build the local user for creation
+   *
+   * @return the ID of the user created, null otherwise
+   */
+  protected function buildLocalUser()
+  {
+    $user_data = array(
+      'name' => $this->uid,
+      'fullname' => "$this->name $this->surname",
+      'password' => $this->generatePassword(),
+      'email' => $this->email,
+      'administrator' => $this->getRoleIdToAssign(),
+      'forcepasswordchange' => 'n',
+      'lastpasschange' => date('Y-m-d H:i:s')
+    );
+    
+    
+    
+    return $user_data;
+  }
+  
+  /**
+   * Return the role to give to the user based on context
+   * If the user is the owner of the app or at least Admin
+   * for each organization, then it is given the role of 'Admin'.
+   * Return 'User' role otherwise
+   *
+   * @return wether the user is administrator or not
+   */
+  public function getRoleIdToAssign() {
+    $role_id = 'no'; // User
+    
+    if ($this->app_owner) {
+      $role_id = 'yes'; // Admin
+    } else {
+      foreach ($this->organizations as $organization) {
+        if ($organization['role'] == 'Admin' || $organization['role'] == 'Super Admin') {
+          $role_id = 'yes';
+        } else {
+          $role_id = 'no';
+        }
+      }
+    }
+    
+    return $role_id;
+  }
   
   /**
    * Get the ID of a local user via Maestrano UID lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function getLocalIdByUid()
-  // {
-  //   $result = $this->connection->query("SELECT ID FROM user WHERE mno_uid = {$this->connection->quote($this->uid)} LIMIT 1")->fetch();
-  //   
-  //   if ($result && $result['ID']) {
-  //     return $result['ID'];
-  //   }
-  //   
-  //   return null;
-  // }
+  protected function getLocalIdByUid()
+  {
+    $sql = sprintf("SELECT id FROM CRMloginusers WHERE mno_uid = '%s'", mysql_real_escape_string($this->uid));
+    $q = mysql_query($sql,$this->connection);
+    
+    if ($q) {
+      $result = mysql_fetch_assoc($q);
+      
+      if ($result && $result['id']) {
+        return $result['id'];
+      }
+    }
+    
+    return null;
+  }
   
   /**
    * Get the ID of a local user via email lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function getLocalIdByEmail()
-  // {
-  //   $result = $this->connection->query("SELECT ID FROM user WHERE email = {$this->connection->quote($this->email)} LIMIT 1")->fetch();
-  //   
-  //   if ($result && $result['ID']) {
-  //     return $result['ID'];
-  //   }
-  //   
-  //   return null;
-  // }
+  protected function getLocalIdByEmail()
+  {    
+    $sql = sprintf("SELECT id FROM CRMloginusers WHERE EMAIL = '%s'", mysql_real_escape_string($this->email));
+    $q = mysql_query($sql,$this->connection);
+    
+    if ($q) {
+      $result = mysql_fetch_assoc($q);
+      
+      if ($result && $result['id']) {
+        return $result['id'];
+      }
+    }
+    
+    return null;
+  }
   
   /**
    * Set all 'soft' details on the user (like name, surname, email)
@@ -122,28 +183,43 @@ class MnoSsoUser extends MnoSsoBaseUser
    *
    * @return boolean whether the user was synced or not
    */
-   // protected function syncLocalDetails()
-   // {
-   //   if($this->local_id) {
-   //     $upd = $this->connection->query("UPDATE user SET name = {$this->connection->quote($this->name . ' ' . $this->surname)}, email = {$this->connection->quote($this->email)} WHERE ID = $this->local_id");
-   //     return $upd;
-   //   }
-   //   
-   //   return false;
-   // }
+   protected function syncLocalDetails()
+   {
+     if($this->local_id) {
+       
+       $sql = sprintf("UPDATE CRMloginusers SET name = '%s',
+        FULLNAME = '%s',
+        EMAIL = '%s'
+        WHERE id = %s", 
+        mysql_real_escape_string($this->uid), 
+        mysql_real_escape_string("$this->name $this->surname"), 
+        mysql_real_escape_string($this->email),
+        mysql_real_escape_string($this->local_id));
+        
+       $upd = mysql_query($sql,$this->connection);
+       
+       return $upd;
+     }
+     
+     return false;
+   }
   
   /**
    * Set the Maestrano UID on a local user via id lookup
    *
    * @return a user ID if found, null otherwise
    */
-  // protected function setLocalUid()
-  // {
-  //   if($this->local_id) {
-  //     $upd = $this->connection->query("UPDATE user SET mno_uid = {$this->connection->quote($this->uid)} WHERE ID = $this->local_id");
-  //     return $upd;
-  //   }
-  //   
-  //   return false;
-  // }
+  protected function setLocalUid()
+  {
+    if($this->local_id) {
+      $sql = sprintf("UPDATE CRMloginusers SET mno_uid = '%s' WHERE id = %s", 
+       mysql_real_escape_string($this->uid),
+       mysql_real_escape_string($this->local_id));
+      
+      $upd = mysql_query($sql,$this->connection);
+      return $upd;
+    }
+    
+    return false;
+  }
 }
